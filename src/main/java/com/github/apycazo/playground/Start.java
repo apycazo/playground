b8ddb59602fae5c1f046f859345b4251f4878cdd
Andres Picazo Cuesta <apycazo@gmail.com>
2016-01-11 22:45:59 +0100

    Some random files to create directories.

---
+++ src/main/java/com/github/apycazo/playground/Start.java
@@ -0,0 +1,12 @@
+package com.github.apycazo.playground;
+
+/**
+ * Created by andres on 11/01/2016.
+ */
+public class Start {
+
+    public static void main (String [] args) {
+
+
+    }
+}
---
+++ src/test/java/com/github/apycazo/test/playground/BasicTest.java
@@ -0,0 +1,8 @@
+package com.github.apycazo.test.playground;
+
+/**
+ * Created by andres on 11/01/2016.
+ */
+public class BasicTest {
+
+}
